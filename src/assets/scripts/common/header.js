d43f51ee4bfed257bf8cc0080e25f764f59406f1
imsashasho <imsashasho@gmail.com>
2023-01-25 20:28:16 +0200

    jj

--- src/assets/scripts/common/header.js
+++ src/assets/scripts/common/header.js
@@ -4,7 +4,7 @@ import { ScrollTrigger } from 'gsap/ScrollTrigger';
 {
   const headerRef = document.querySelector('.header');
   const menuRef = document.getElementById('menu');
-  const toggleBtnRef = document.getElementById('toggle-menu');
+  const toggleBtnRef = document.getElementById('header-toggle-menu');
 
   gsap.registerPlugin(ScrollTrigger);
   window.addEventListener('scroll', evt => {
@@ -14,16 +14,16 @@ import { ScrollTrigger } from 'gsap/ScrollTrigger';
     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
       headerRef.classList.add('bg-header');
       headerRef.classList.remove('on');
-      // toggleBtnRef.classList.remove('on');
+      toggleBtnRef.classList.remove('on');
     } else {
       headerRef.classList.remove('bg-header');
     }
   }
 
-//   const toggleMenu = () => {
-//     toggleBtnRef.classList.toggle('on');
-//     headerRef.classList.toggle('on');
-//   };
+  const toggleMenu = () => {
+    toggleBtnRef.classList.toggle('on');
+    headerRef.classList.toggle('on');
+  };
 
-//   toggleBtnRef.addEventListener('click', toggleMenu);
+  toggleBtnRef.addEventListener('click', toggleMenu);
 }
