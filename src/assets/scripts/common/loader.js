4082786c52fdf9fd0f5c5f14622e55f7b896885d
yatsinoa <sashayatzino@gmail.com>
2023-01-26 03:32:44 +0200

    loader

--- src/assets/scripts/common/loader.js
+++ src/assets/scripts/common/loader.js
@@ -1,32 +1,32 @@
-// import gsap from 'gsap';
+import gsap from 'gsap';
 
-// const preloaderRef = document.querySelector('.preloader');
-// export const preloader = {
-//   el: preloaderRef,
-//   subscribers: [],
+const preloaderRef = document.querySelector('.preloader');
+export const preloader = {
+  el: preloaderRef,
+  subscribers: [],
 
-//   remove() {
-//     if (preloaderRef) {
-//       gsap.to(preloaderRef, {
-//         opacity: 0,
-//         duration: 0.5,
-//         onComplete: () => {
-//           console.log(preloader);
-//           this.subscribers.forEach(fn => fn());
-//           this.el.remove();
-//         },
-//       });
-//     }
-//   },
-//   onRemove(fn) {
-//     this.subscribers.push(fn);
-//   },
-// };
+  remove() {
+    if (preloaderRef) {
+      gsap.to(preloaderRef, {
+        opacity: 0,
+        duration: 0.5,
+        onComplete: () => {
+          console.log(preloader);
+          this.subscribers.forEach(fn => fn());
+          this.el.remove();
+        },
+      });
+    }
+  },
+  onRemove(fn) {
+    this.subscribers.push(fn);
+  },
+};
 
-// window.addEventListener('load', () => {
-//   setTimeout(() => {
-//     preloader.remove();
-//   }, 100);
-// });
+window.addEventListener('load', () => {
+  setTimeout(() => {
+    preloader.remove();
+  }, 100);
+});
 
-// export default preloader;
+export default preloader;
--- src/assets/scripts/index-app.js
+++ src/assets/scripts/index-app.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import './common/header';
 import './common/footer';
-// import { preloader } from './common/loader';
+import { preloader } from './common/loader';
 
 global.gsap = gsap;
 global.axios = axios;
