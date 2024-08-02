b261217d6056647e8b155751d4869298c9c049ca
Dron <dron_2019@ukr.net>
2024-08-02 11:37:02 +0300

    loyalty block

--- src/assets/scripts/loyalty/index.js
+++ src/assets/scripts/loyalty/index.js
@@ -9,63 +9,58 @@ cardsContainer.addEventListener('click', e => {
 
   if (!activeCard) return;
 
-  cards.forEach(card => card.classList.remove('loyalty-item__active'));
-  cards.forEach((card, i) => card.setAttribute('data-position', i + 1));
+  if (activeCard.classList.contains('loyalty-item__active')) {
+    activeCard.classList.remove('loyalty-item__active');
+    popupWithLi.style.display = 'none';
+    return;
+  }
+  const $container = document.querySelector('.loyalty-row');
+  const $cards = $container.querySelectorAll('.loyalty-item');
 
-  // Main Cards
-  // Ширина одного эемента
-  const elementWidth = activeCard.offsetWidth;
-  // Высота одного эемента
-  const elementHeight = activeCard.offsetHeight;
-  // Позиция элемента в ряду
-  const positionElementInRow = activeCard.getAttribute('data-position');
-  // Кол-во элементов в ряду
-  const countElementsInOneRow = Math.round(cardsContainer.offsetWidth / elementWidth);
-  // Позиция элемента в колонке
-  const positionElementInColumn = Math.ceil(positionElementInRow / countElementsInOneRow);
-  //____________________________________________________
-  // console.log('Количество єлементов в одном ряду: ', countElementsInOneRow);
-  // console.log('Количество столбцов: ', Math.round(cardsContainer.offsetHeight / elementHeight));
-  // console.log('Позиция элемента в колонке: ', positionElementInColumn);
-  // console.log('Позиция элемента: ', positionElementInRow);
-  // console.log('Ширина элемента: ', elementWidth);
-  // console.log('Высота элемента: ', elementHeight);
-  // _____________________________________________________
-  // Main Cards End
+  
+  const indexOfActiveCard = Array.from(cards).indexOf(activeCard);
+  const $popup = $container.querySelector('.loyalty-item__list');
+  
+  const howManyCardsInRow = Math.round(getComputedStyle($container).gridTemplateColumns.split(' ').length);
 
-  // ------ LI in Info Row -----------
-  // Количество li в попапе
-  const countLiInPopup = popupWithLi.querySelector('.loyalty-info-row').children.length;
-  // Ширина попапа
-  const popupListWidth = cardsContainer.offsetWidth;
-  // Ширина одного элемента
-  const popupItemWidth = popupList.children[0].offsetWidth;
-  const popupItemHeight = popupList.children[0].offsetHeight;
-  // Кол-во элементов в ряду
-  const countPopupElementsInOneRow = Math.round(cardsContainer.offsetWidth / popupItemWidth);
-  // Количество колонок
-  const countColumnsInPopupList = Math.round(popupList.offsetHeight / popupItemHeight);
+  const inWhichRowIsActiveCard = Math.ceil((indexOfActiveCard + 1) / howManyCardsInRow);
 
-  // console.log('____________________________');
+  const rowEnd = inWhichRowIsActiveCard * howManyCardsInRow;
+  const isLastRow = rowEnd > $cards.length;
 
-  const test = 4 * countColumnsInPopupList;
-  const test2 = Math.round((popupItemHeight / elementHeight) * countColumnsInPopupList) + 2;
-  console.log('popupList.offsetHeight: ', popupList.offsetHeight);
-  console.log('popupItemHeight ', popupItemHeight);
-  console.log('rowEnd ', test2);
+  if (isLastRow) {
+    $container.insertAdjacentElement('beforeend', $popup);
+  } else {
+    const $nextCard = $cards[rowEnd-1];
+    $nextCard.insertAdjacentElement('afterend', $popup);
+  }
 
-  // Warn End
-  popupWithLi.style.display = 'grid';
-  popupWithLi.style.gridColumnStart = 1;
-  popupWithLi.style.gridColumnEnd = countElementsInOneRow + 1;
-  popupWithLi.style.gridRowStart = positionElementInColumn + 1;
-  popupWithLi.style.gridRowEnd = test2;
+  $popup.style.gridColumnStart = 1;
+  $popup.style.gridColumnEnd = howManyCardsInRow + 1;
+  $popup.style.gridRowStart = inWhichRowIsActiveCard + 1;
+  $popup.style.gridRowEnd = inWhichRowIsActiveCard + 1;
+  $popup.style.display = 'block';
+
+  $cards.forEach(card => card.classList.remove('loyalty-item__active'));
 
   activeCard.classList.add('loyalty-item__active');
+
+  console.log(getComputedStyle($container).gridTemplateColumns.split(' '));
+  console.log('isLastRow', isLastRow);
+  console.log('rowEnd', rowEnd);
+  console.log('howManyCardsInRow', howManyCardsInRow);
+  console.log('inWhichRowIsActiveCard', inWhichRowIsActiveCard);
+  
+  console.log('indexOfActiveCard:', indexOfActiveCard);
+
+  
+  
+
 });
 
-// let style = window.getComputedStyle(activeCard);
-// let cardwidth = style.getPropertyValue('width');
-// let cardheight = style.getPropertyValue('height');
-// console.log('cardwidth:', cardwidth);
-// console.log('cardheight:', cardheight);
+
+window.addEventListener('resize', () => {
+  document.querySelectorAll('.loyalty-item__active').forEach(card => {
+    card.click();
+  });
+});
